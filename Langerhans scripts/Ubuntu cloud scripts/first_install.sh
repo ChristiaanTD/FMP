221c82c3b59c988444f52ef2fa2756457efcc0f2
ChristiaanTD <t.c.dormans@gmail.com>
2024-09-09 16:10:22 +0200

    Upload for final version FMP

--- Langerhans scripts/Ubuntu cloud scripts/first_install.sh
+++ Langerhans scripts/Ubuntu cloud scripts/first_install.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-# Update package list
+#-------------This script can be used if necessary as a first install on your cloud server to intstall Nextflow and Docker-----------#
+
+# first update package list
 sudo apt-get update
 
-# Install Java (required for Nextflow)
+# next we install Java (if you don't have it installed already)
 sudo apt-get install -y openjdk-11-jdk
 
-# Install Nextflow
+# install Nextflow
 curl -s https://get.nextflow.io | bash
 sudo mv nextflow /usr/local/bin/
 
@@ -14,7 +16,7 @@ sudo mv nextflow /usr/local/bin/
 nextflow -v
 
 
-# Install Docker
+# install Docker 
 sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
@@ -22,11 +24,11 @@ sudo apt-get update
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 
 
-# Verify Docker installation
+# verify Docker installation
 sudo systemctl status docker
 
-# Add user to group
+# add user to group
 sudo usermod -aG docker $USER 
 
-# Print message for user to logout and login back
+# print message so you don't forget to log out and back on for group changes to take effect!
 echo "Installation complete. Please log out and log back in for the group changes to take effect."
